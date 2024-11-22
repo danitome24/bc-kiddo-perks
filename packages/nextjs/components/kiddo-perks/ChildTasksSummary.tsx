c84dd18e90d4a784ac9d0c45558c7930a902269e
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-11-22 22:42:10 +0100

    Used image component from next

--- packages/nextjs/components/kiddo-perks/ChildTasksSummary.tsx
+++ packages/nextjs/components/kiddo-perks/ChildTasksSummary.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 export const ChildTasksSummary = () => {
   return (
     <div className="flex flex-col gap-4 border-solid border-2 border-black rounded-md bg-secondary">
       <div className="flex flex-row justify-center items-center py-6 px-3">
-        <img src="https://via.placeholder.com/64" alt="Child picture" className="w-16 h-16 rounded-full" />
+        <Image src="/childAvatar.png" width={64} height={64} alt="Child picture" className="w-16 h-16 rounded-full" />
         <h4 className="px-4 font-semibold">Anna</h4>
       </div>
       <div className="flex flex-col px-8">
