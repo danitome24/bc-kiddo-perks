5a0ef793b66b247650ee515b532e7eb69e8e66b1
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-11-23 07:33:01 +0100

    Renamed script deployment

--- packages/foundry/script/DeployKiddoPerks.s.sol
+++ packages/foundry/script/DeployKiddoPerks.s.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../contracts/KiddoPerks.sol";
-import "./DeployHelpers.s.sol";
+import { KiddoPerks } from "../contracts/KiddoPerks.sol";
+import { ScaffoldETHDeploy, console } from "./DeployHelpers.s.sol";
 
-contract DeployYourContract is ScaffoldETHDeploy {
+contract DeployKiddoPerks is ScaffoldETHDeploy {
   // use `deployer` from `ScaffoldETHDeploy`
   function run() external ScaffoldEthDeployerRunner {
     address PARENT = 0x27dBc64e6C38633eD526d970258372476BCE58C0;
@@ -12,7 +12,7 @@ contract DeployYourContract is ScaffoldETHDeploy {
     KiddoPerks kiddoPerks = new KiddoPerks(PARENT);
     console.logString(
       string.concat(
-        "YourContract deployed at: ", vm.toString(address(kiddoPerks))
+        "KiddoPerks deployed at: ", vm.toString(address(kiddoPerks))
       )
     );
   }
