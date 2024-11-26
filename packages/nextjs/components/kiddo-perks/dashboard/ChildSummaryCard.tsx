d323a62468c40a3412bb87441c44f7ae1a69fc93
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-11-26 15:18:09 +0100

    Reduced width for child summary card

--- packages/nextjs/components/kiddo-perks/dashboard/ChildSummaryCard.tsx
+++ packages/nextjs/components/kiddo-perks/dashboard/ChildSummaryCard.tsx
@@ -7,18 +7,17 @@ type ChildSummaryCardProps = {
 
 export const ChildSummaryCard = ({ child }: ChildSummaryCardProps) => {
   return (
-    <div className="card bg-primary w-60 shadow-xl">
+    <div className="card bg-primary shadow-xl">
       <div className="card-body">
         <h2 className="card-title">
           <Image className="w-16 h-16 rounded-full" width={90} height={90} src={child.avatar} alt={child.name} />
           {child.name}
         </h2>
-        <p className="text-lg text-right m-0">{child.tokens} KDO</p>
-        <progress
-          className="progress progress-neutral"
-          value={child.progress?.completed}
-          max={child.progress?.total}
-        ></progress>
+        <p className="text-lg text-right m-0">{child.tokens}KDO</p>
+        <p className="m-0">
+          Tasks completed: {child.progress?.completed}/{child.progress?.total}
+        </p>
+
         <div className="card-actions justify-end">
           <button className="btn btn-secondary btn-sm">See child</button>
         </div>
