66d571fe4d52d3f557a042fe93bb8b4cb37e6af5
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-12-01 23:06:50 +0100

    Display a message if no tasks are added

--- packages/nextjs/components/kiddo-perks/dashboard/TasksList.tsx
+++ packages/nextjs/components/kiddo-perks/dashboard/TasksList.tsx
@@ -23,6 +23,17 @@ export const TasksList = () => {
     }
   }, [currentTasks]);
 
+  if (tasks.length === 0) {
+    return (
+      <section className="mt-6">
+        <h2 className="text-xl font-semibold mb-4">Your Tasks</h2>
+        <div className="bg-primary shadow-md rounded-lg divide-y divide-base">
+          <p className="py-8 px-4 text-center">No tasks to be done</p>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section className="mt-6">
       <h2 className="text-xl font-semibold mb-4">Your Tasks</h2>
