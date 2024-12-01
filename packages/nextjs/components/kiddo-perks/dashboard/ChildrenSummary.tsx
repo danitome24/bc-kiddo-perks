7d155125b83b27f3a897bfae1c115314760173c4
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-12-01 21:28:36 +0100

    Display a message if no children is added

--- packages/nextjs/components/kiddo-perks/dashboard/ChildrenSummary.tsx
+++ packages/nextjs/components/kiddo-perks/dashboard/ChildrenSummary.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { ChildSummaryCard } from "./ChildSummaryCard";
 import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 import { Child } from "~~/types/kiddoPerks";
@@ -12,8 +13,24 @@ export const ChildrenSummary = () => {
     return <section>Loading...</section>;
   }
 
+  if (childrenData.length === 0) {
+    return (
+      <section>
+        <h2 className="text-lg font-semibold text-primary-content mb-4">Children Status</h2>
+        <div className="grid grid-cols-1 gap-4">
+          <div className="flex flex-col justify-center items-center bg-primary rounded-xl py-4 px-8">
+            <p className="text-center  w-full h-full">Add a child</p>
+            <Link className="btn-sm btn btn-success" href="/childs">
+              Go
+            </Link>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
   return (
-    <section className="">
+    <section>
       <h2 className="text-lg font-semibold text-primary-content mb-4">Children Status</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {childrenData.map(child => (
